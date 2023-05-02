d81cd57e3cb7939dc0924edc16e342dc1cd9192b
Matthew Stroud <mbstroud@gmail.com>
2023-05-02 16:04:45 -0400

    new changes

---
+++ scripts/playlists.js
@@ -0,0 +1,57 @@
+import { numbers } from './numbers.js';
+import { settings } from './settings.js';
+export let playlists = {
+    "start": function _start(playListId, random) {
+        debugger;
+        let combatPlaylist = game.playlists.get(playListId);
+        if (combatPlaylist) {
+            if (combatPlaylist.playing) {
+                return;
+            }
+            if (random){
+                let songs = combatPlaylist.playbackOrder;
+                let songIndex = numbers.getRandomInt(0, songs.length);
+                combatPlaylist.playNext(songs[songIndex]);
+            }
+            combatPlaylist.playAll();
+        }
+    },
+    "toggle": function _toggle(playlistId, random) {
+        let combatPlaylist = game.playlists.get(playlistId);
+        if (combatPlaylist) {
+            if (combatPlaylist.playing) {
+                combatPlaylist.stopAll();
+                return;
+            }
+            if (random){
+                let songs = combatPlaylist.playbackOrder;
+                let songIndex = numbers.getRandomInt(0, songs.length);
+                combatPlaylist.playNext(songs[songIndex]);
+            }
+            combatPlaylist.playAll();
+        }
+    }
+};
+export let music = {
+    "combat": {
+        "start": function _start(random){
+            random = random ?? true;
+            let combatPlaylistId = settings.CombatPlaylist.getValue();
+            if (!combatPlaylistId || combatPlaylistId == "none"){
+                ui.notifications.notify('You must first select a combat playlist under settings.');
+                return;
+            }
+            playlists.start(combatPlaylistId, random);
+        },
+        "toggle": function _toggle(random){
+            debugger;
+            random = random ?? true;
+            let combatPlaylistId = settings.CombatPlaylist.getValue();
+            if (!combatPlaylistId || combatPlaylistId == "none"){
+                ui.notifications.notify('You must first select a combat playlist under settings.');
+                return;
+            }
+            playlists.toggle(combatPlaylistId, random);
+        }
+    }
+}
