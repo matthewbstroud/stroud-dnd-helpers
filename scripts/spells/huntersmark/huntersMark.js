8e74262c2f7196b63835b91b6c35f97e1ae1cf38
Matthew Stroud <mbstroud@gmail.com>
2023-05-05 10:48:29 -0400

    finish hunter's mark

--- scripts/spells/huntersmark/huntersMark.js
+++ scripts/spells/huntersmark/huntersMark.js
@@ -1,173 +1,242 @@
-import { gmFunctions, tokenOrActor } from "../../gm/gmFunctions.js";
+import { gmFunctions, getTokenOrActor } from "../../gm/gmFunctions.js";
+import { getSpellData } from "../spells.js";
 
+const HUNTERS_MARK = "Hunter's Mark";
 
 export let huntersMark = {
-    "itemMacro": async function _itemMacro(speaker, actor, token, character, item, args) {
-        // onUse macro
-        if (args[0].hitTargets.length === 0) return;
-        if (args[0].tag === "OnUse") {
-            const targetUuid = args[0].hitTargets[0].uuid;
-            const tokenOrActor = await fromUuid(args[0].actorUuid);
-            const caster = tokenOrActor.actor ? tokenOrActor.actor : tokenOrActor;
+    "castOrUse": _castOrUse,
+    "itemMacro": _itemMacro,
+    "removeMarks": _removeMarks
+};
 
-            if (!caster || !targetUuid) {
-                ui.notifications.warn("Hunter's Mark: no token/target selected");
-                console.error("Hunter's Mark: no token/target selected");
-                return;
-            }
+async function _removeMarks(caster) {
+    var huntersMarkItem = caster.items.getName(HUNTERS_MARK);
+    if (!huntersMarkItem) {
+        return;
+    }
+    let uuids = canvas.scene.tokens
+        .filter(t => t.id != caster.id)
+        .map(t => t.actor.effects.contents)
+        .reduce((l, r) => l.concat(r)).filter(e => e.origin == huntersMarkItem.uuid && e.label == "Marked")
+        .map(e => e.uuid);
+    gmFunctions.removeEffects(uuids);
+}
 
-            const effectData = {
-                "label": args[0].item.name,
-                "icon": args[0].item.img,
-                "changes": [
-                    {
-                        "key": "flags.midi-qol.huntersMark",
-                        "mode": CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
-                        "value": targetUuid,
-                        "priority": 20
-                    },
-                    {
-                        "key": "flags.dnd5e.DamageBonusMacro",
-                        "mode": CONST.ACTIVE_EFFECT_MODES.CUSTOM,
-                        "value": `ItemMacro.${args[0].item.name}`,
-                        "priority": 20
-                    }
-                ],
-                "transfer": false,
-                "disabled": false,
-                "duration": {
-                    "startTime": null,
-                    "seconds": 3600,
-                    "combat": null,
-                    "rounds": null,
-                    "turns": null,
-                    "startRound": null,
-                    "startTurn": null
-                },
-                "origin": args[0].itemUuid,
-                "tint": null,
-                "flags": {
-                    "dae": {
-                        "selfTarget": false,
-                        "selfTargetAlways": true,
-                        "stackable": "none",
-                        "durationExpression": "",
-                        "macroRepeat": "none",
-                        "specialDuration": []
-                    },
-                    "core": {
-                        "statusId": ""
-                    }
-                }
-            };
+async function _itemMacro(speaker, actor, token, character, item, args) {
+    // onUse macro
+    if (args[0].hitTargets.length === 0) return;
+    if (args[0].tag === "OnUse") {
+        const targetUuid = args[0].hitTargets[0].uuid;
+        const caster = await getTokenOrActor(args[0].actorUuid);
 
-            effectData.duration.startTime = game.time.worldTime;
-            await caster.createEmbeddedDocuments("ActiveEffect", [effectData]);
-        } else if (args[0].tag === "DamageBonus") {
-            // only weapon attacks
-            if (!["mwak", "rwak"].includes(args[0].item.data.actionType)) return {};
-            const targetUuid = args[0].hitTargets[0].uuid;
-            // only on the marked target
-            let currentTargetUuid = args[0].actor?.document?.getFlag("midi-qol", "huntersMark") ?? args[0]?.actor.getFlag("midi-qol", "huntersMark");
-            if (targetUuid !== currentTargetUuid) return {};
-            const damageType = args[0].item.system.damage.parts[0][1];
-            const diceMult = args[0].isCritical ? 2 : 1;
-            return { damageRoll: `${diceMult}d6[${damageType}]`, flavor: "Hunters Mark Damage" };
+        if (!caster || !targetUuid) {
+            ui.notifications.warn("Hunter's Mark: no token/target selected");
+            console.error("Hunter's Mark: no token/target selected");
+            return;
         }
-    },
-    "moveMark": async function _moveMark(originID, oldTargetUuid, newTargetUuid) {
-        debugger;
-        let markedTarget = await tokenOrActor(oldTargetUuid);
-        let priorEffect = markedTarget?.effects?.find(e => e.label == "Marked" && e.origin == originID);
-        if (priorEffect) {
-            await gmFunctions.removeEffects([priorEffect.uuid]);
-        }
-        let effectData = {
-            "label": "Marked",
+
+        const effectData = {
+            "label": args[0].item.name,
+            "icon": args[0].item.img,
             "changes": [
                 {
-                    "key": "StatusEffectLabel",
-                    "value": "Marked",
-                    "mode": 0,
+                    "key": "flags.midi-qol.huntersMark",
+                    "mode": CONST.ACTIVE_EFFECT_MODES.OVERRIDE,
+                    "value": targetUuid,
+                    "priority": 20
+                },
+                {
+                    "key": "flags.dnd5e.DamageBonusMacro",
+                    "mode": CONST.ACTIVE_EFFECT_MODES.CUSTOM,
+                    "value": `ItemMacro.${args[0].item.name}`,
                     "priority": 20
                 }
             ],
+            "transfer": false,
+            "disabled": false,
             "duration": {
-                "seconds": 3600,
                 "startTime": null,
+                "seconds": 3600,
                 "combat": null,
                 "rounds": null,
                 "turns": null,
-                "startRound": 0,
-                "startTurn": 0
+                "startRound": null,
+                "startTurn": null
             },
+            "origin": args[0].itemUuid,
             "tint": null,
-            "transfer": false,
-            "disabled": false,
             "flags": {
                 "dae": {
-                    "transfer": false,
-                    "stackable": "none",
                     "selfTarget": false,
-                    "selfTargetAlways": false,
+                    "selfTargetAlways": true,
+                    "stackable": "none",
                     "durationExpression": "",
                     "macroRepeat": "none",
                     "specialDuration": []
                 },
-                "ddbimporter": {
-                    "disabled": false
-                },
-                "midi-qol": {
-                    "forceCEOff": true,
-                    "castData": {
-                        "baseLevel": 1,
-                        "castLevel": 1,
-                        "itemUuid": `${originID}`
-                    }
-                },
-                "dfreds-convenient-effects": {
-                    "description": "Marked by a skilled Hunter"
-                },
                 "core": {
                     "statusId": ""
                 },
-                "ActiveAuras": {
-                    "isAura": false,
-                    "aura": "None",
-                    "radius": "undefined",
-                    "alignment": "",
-                    "type": "",
-                    "ignoreSelf": false,
-                    "height": false,
-                    "hidden": false,
-                    "displayTemp": false,
-                    "hostile": false,
-                    "onlyOnce": false
-                },
-                "times-up": {
-                    "isPassive": false
+                "effectmacro": {
+                    "onDelete": {
+                        "script": "stroudDnD.spells.HuntersMark.removeMarks(origin.parent);"
+                    }
+                }
+            }
+        };
+
+        effectData.duration.startTime = game.time.worldTime;
+        await caster.createEmbeddedDocuments("ActiveEffect", [effectData]);
+    } else if (args[0].tag === "DamageBonus") {
+        // only weapon attacks
+        if (!["mwak", "rwak"].includes(args[0].item.system.actionType)) return {};
+        const targetUuid = args[0].hitTargets[0].uuid;
+        // only on the marked target
+        let currentTargetUuid = args[0].actor?.document?.getFlag("midi-qol", "huntersMark") ?? args[0]?.actor.getFlag("midi-qol", "huntersMark");
+        if (targetUuid !== currentTargetUuid) return {};
+        const damageType = args[0].item.system.damage.parts[0][1];
+        const diceMult = args[0].isCritical ? 2 : 1;
+        return { damageRoll: `${diceMult}d6[${damageType}]`, flavor: "Hunters Mark Damage" };
+    }
+}
+
+async function _castOrUse() {
+    let spellData = await getSpellData(HUNTERS_MARK);
+    if (!spellData) {
+        return;
+    }
+    if (spellData.targets.length != 1) {
+        ui.notifications.notify(`You must target a single creature!`);
+        return;
+    }
+    var selfEffect = spellData.actor.effects.find(e => e.label == HUNTERS_MARK && e.origin == spellData.item.uuid);
+    if (!selfEffect) {
+        spellData.item.roll();
+        return;
+    }
+
+    let targetID = spellData.targets[0].document.uuid;
+
+    let markedTargetID = spellData.actor.getFlag("midi-qol", "huntersMark");
+    if (!markedTargetID) {
+        ui.notifications.notify(`You do not have a marked target!`);
+        return;
+    }
+
+    if (targetID == markedTargetID) {
+        ui.notifications.notify(`This target is already marked!`);
+        return;
+    }
+
+    let markedTarget = await getTokenOrActor(markedTargetID);
+    if (markedTarget && markedTarget.system.attributes.hp.value > 0) {
+        ui.notifications.notify(`${markedTarget.name} must be dead before you can move your mark!`);
+        return;
+    }
+
+    await selfEffect.update({
+        "changes": [
+            {
+                "key": "flags.midi-qol.huntersMark",
+                "mode": 5,
+                "value": `${targetID}`,
+                "priority": 20
+            },
+            {
+                "key": "flags.dnd5e.DamageBonusMacro",
+                "mode": 0,
+                "value": `ItemMacro.${HUNTERS_MARK}`,
+                "priority": 20
+            }
+        ]
+    });
+
+    await _moveMark(spellData.item.uuid, markedTargetID, targetID);
+}
+
+async function _moveMark(originID, oldTargetUuid, newTargetUuid) {
+    let markedTarget = await getTokenOrActor(oldTargetUuid);
+    let priorEffect = markedTarget?.effects?.find(e => e.label == "Marked" && e.origin == originID);
+    if (priorEffect) {
+        await gmFunctions.removeEffects([priorEffect.uuid]);
+    }
+    let effectData = createMarkedEffect(originID);
+    let target = await fromUuid(newTargetUuid);
+    if (!target) {
+        ui.notifications.error(`Target doesn't exist!`);
+        return;
+    }
+    await gmFunctions.createEffects(newTargetUuid, [effectData]);
+}
+
+function createMarkedEffect(originID) {
+    return {
+        "label": "Marked",
+        "changes": [
+            {
+                "key": "StatusEffectLabel",
+                "value": "Marked",
+                "mode": 0,
+                "priority": 20
+            }
+        ],
+        "duration": {
+            "seconds": 3600,
+            "startTime": null,
+            "combat": null,
+            "rounds": null,
+            "turns": null,
+            "startRound": 0,
+            "startTurn": 0
+        },
+        "tint": null,
+        "transfer": false,
+        "disabled": false,
+        "flags": {
+            "dae": {
+                "transfer": false,
+                "stackable": "none",
+                "selfTarget": false,
+                "selfTargetAlways": false,
+                "durationExpression": "",
+                "macroRepeat": "none",
+                "specialDuration": []
+            },
+            "ddbimporter": {
+                "disabled": false
+            },
+            "midi-qol": {
+                "forceCEOff": true,
+                "castData": {
+                    "baseLevel": 1,
+                    "castLevel": 1,
+                    "itemUuid": `${originID}`
                 }
             },
-            "icon": "icons/weapons/bows/shortbow-recurve-yellow.webp",
-            "origin": `${originID}`
-        };
-        let target = await fromUuid(newTargetUuid);
-        if (!target) {
-            ui.notifications.error(`Target doesn't exist!`);
-            return;
-        }
-        await gmFunctions.createEffects(newTargetUuid, [effectData]);
-    },
-    "removeMarks": async function _removeMarks(caster) {
-        var huntersMarkItem = caster.items.getName("Hunter's Mark");
-        if (!huntersMarkItem) {
-            return;
-        }
-        let uuids = canvas.scene.tokens
-            .filter(t => t.id != caster.id)
-            .map(t => t.actor.effects.contents)
-            .reduce((l, r) => l.concat(r)).filter(e => e.data.origin == huntersMarkItem.uuid && e.data.label == "Marked")
-            .map(e => e.uuid);
-        gmFunctions.removeEffects(uuids);
-    }
-};
+            "dfreds-convenient-effects": {
+                "description": "Marked by a skilled Hunter"
+            },
+            "core": {
+                "statusId": ""
+            },
+            "ActiveAuras": {
+                "isAura": false,
+                "aura": "None",
+                "radius": "undefined",
+                "alignment": "",
+                "type": "",
+                "ignoreSelf": false,
+                "height": false,
+                "hidden": false,
+                "displayTemp": false,
+                "hostile": false,
+                "onlyOnce": false
+            },
+            "times-up": {
+                "isPassive": false
+            }
+        },
+        "icon": "icons/weapons/bows/shortbow-recurve-yellow.webp",
+        "origin": `${originID}`
+    };
+}
