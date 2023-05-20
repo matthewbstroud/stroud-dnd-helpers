40dfa071af3c614833eaa2b667d6ede1d194e0e5
Matthew Stroud <mbstroud@gmail.com>
2023-05-20 09:23:07 -0400

    ensure resource link clear targets after use

--- scripts/spells/twilightDomain/twilightDomain.js
+++ scripts/spells/twilightDomain/twilightDomain.js
@@ -2,69 +2,22 @@ import { dialog } from "../../dialog/dialog.js";
 import { gmFunctions } from "../../gm/gmFunctions.js";
 import { sdndConstants } from "../../constants.js";
 
-export let twilightDomain = {
-    "turnEnd": async function _onTurnEnd(casterUuid, actorUuid){
-        let caster = await gmFunctions.getTokenOrActor(casterUuid);
-        let actor = await gmFunctions.getTokenOrActor(actorUuid);
-        if (!caster || !actor) {
-            return;
-        }
-        if (twilightUtil.shouldTerminate(caster)) {
-            await twilightUtil.removeTwilightEffects(caster);
-            return;
-        }
-        
-        if (twilightUtil.getDistance(caster, actor) > 30) {
-            return;
-        }
-        
-        let choice = await twilightUtil.getUserChoice(actor);
-        switch(choice) {
-            case "heal":
-                await twilightUtil.applyTempHP(caster, actor);
-                break;
-            default:
-                await twilightUtil.removeEffect(actor, choice);
-        }
-    },
-    "finalTurn": async function _finalTurn(casterUuid, actorUuid){
-        if (game.combats.active) {
-            // if the effect fell off in combat, no final heal
-            return;
-        }
-
-        let caster = await gmFunctions.getTokenOrActor(casterUuid);
-        if (!caster) {
-            return;
-        }
-
-        let actor = await gmFunctions.getTokenOrActor(actorUuid);
-        if (!actor) {
-            return;
-        }
-        
-        if (twilightUtil.shouldTerminate(caster)) {
-            return;
-        }
-        
-        if (twilightUtil.getDistance(caster, actor) > 30) {
-            return;
-        }
-        
-        await twilightUtil.applyTempHP(caster, actor);        
-    },
-    "removeTwilightEffects": async function _removeTwilightEffects(casterUuid) {
-        let caster = await gmFunctions.getTokenOrActor(casterUuid);
-        if (!caster) {
-            return;
-        }
-
-        await twilightUtil.removeTwilightEffects(caster);
-    }
-};
-
-
 const twilightUtil = {
+    ensureResourceLink: async function _ensureResourceLink(actor, item) {
+        let channelDivinity = actor.items.getName("Channel Divinity");
+        if (!channelDivinity) {
+            return;
+        }
+        if (item.system.consume.target != channelDivinity.id) {
+            await item.update({
+                "system.consume": {
+                    "amount": 1,
+                    "target": `${channelDivinity.id}`,
+                    "type": "charges"
+                }
+            });
+        }
+    },
     getDistance: function _getDistance(source, target) {
         let sourceTokens = source.getActiveTokens();
         let targetTokens = target.getActiveTokens();
@@ -93,12 +46,12 @@ const twilightUtil = {
                 .map(e => ({ label: `Remove: ${e.label}`, value: e.uuid })));
         return await dialog.createButtonDialog(sdndConstants.FEATURES.TWILIGHT_SANCTUARY, buttons, "column");
     },
-    applyTempHP: async function _applyTempHP(caster, target){
+    applyTempHP: async function _applyTempHP(caster, target) {
         let current_tempHP = target.system?.attributes?.hp?.temp;
-    
+
         // Roll Twilight Sanctuary temporary hit points
         let healRoll = new Roll('1d6 + @classes.cleric.levels', caster.getRollData()).evaluate({ async: false });
-    
+
         healRoll.toMessage({
             user: game.user._id,
             speaker: ChatMessage.getSpeaker(),
@@ -107,11 +60,11 @@ const twilightUtil = {
         // Check if new roll is higher than old temp HP
         console.log(healRoll);
         let new_tempHP = parseInt(healRoll.total);
-    
+
         if (current_tempHP && current_tempHP >= new_tempHP) {
             return;
         }
-    
+
         await target.update({ 'system.attributes.hp.temp': new_tempHP });
     },
     removeEffect: async function _removeEffect(effectID) {
@@ -119,7 +72,7 @@ const twilightUtil = {
     },
     shouldTerminate: function _shouldTerminate(caster) {
         return caster.system.attributes.hp.value <= 0 || caster.effects.contents
-                    .filter(e => e.label.match(/(unconscious|incapacitated)/gi)).length > 0;
+            .filter(e => e.label.match(/(unconscious|incapacitated)/gi)).length > 0;
     },
     removeTwilightEffects: async function _removeTwilightEffects(caster) {
         let twilightEffects = [];
@@ -128,9 +81,83 @@ const twilightUtil = {
                 let removeEffects = t.actor.effects.contents.filter(e => e.origin?.startsWith(`Actor.${caster.id}`) && e.label.match(/(twilight sanctuary \(sdnd\)|tsaura)/gi));
                 twilightEffects = twilightEffects.concat(removeEffects);
             });
-        if (!twilightEffects || twilightEffects.length == 0){
+        if (!twilightEffects || twilightEffects.length == 0) {
             return;
         }
         await gmFunctions.removeEffects(twilightEffects.map(e => e.uuid));
+    },
+    itemMacro: async function _itemMacro(speaker, actor, token, character, item, args) {
+        if (args[0]?.macroPass == "preItemRoll") {
+            await twilightUtil.ensureResourceLink(actor, item);
+        }
+        else if (args[0]?.macroPass == "postActiveEffects") {
+            game.user.updateTokenTargets();
+        }
     }
 };
+
+export let twilightDomain = {
+    "itemMacro": twilightUtil.itemMacro,
+    "turnEnd": async function _onTurnEnd(casterUuid, actorUuid) {
+        let caster = await gmFunctions.getTokenOrActor(casterUuid);
+        let actor = await gmFunctions.getTokenOrActor(actorUuid);
+        if (!caster || !actor) {
+            return;
+        }
+        if (twilightUtil.shouldTerminate(caster)) {
+            await twilightUtil.removeTwilightEffects(caster);
+            return;
+        }
+
+        if (twilightUtil.getDistance(caster, actor) > 30) {
+            return;
+        }
+
+        let choice = await twilightUtil.getUserChoice(actor);
+        switch (choice) {
+            case "heal":
+                await twilightUtil.applyTempHP(caster, actor);
+                break;
+            default:
+                await twilightUtil.removeEffect(actor, choice);
+        }
+    },
+    "finalTurn": async function _finalTurn(casterUuid, actorUuid) {
+        if (game.combats.active) {
+            // if the effect fell off in combat, no final heal
+            return;
+        }
+
+        let caster = await gmFunctions.getTokenOrActor(casterUuid);
+        if (!caster) {
+            return;
+        }
+
+        let actor = await gmFunctions.getTokenOrActor(actorUuid);
+        if (!actor) {
+            return;
+        }
+
+        if (twilightUtil.shouldTerminate(caster)) {
+            return;
+        }
+
+        if (twilightUtil.getDistance(caster, actor) > 30) {
+            return;
+        }
+
+        await twilightUtil.applyTempHP(caster, actor);
+    },
+    "removeTwilightEffects": async function _removeTwilightEffects(casterUuid) {
+        let caster = await gmFunctions.getTokenOrActor(casterUuid);
+        if (!caster) {
+            return;
+        }
+
+        await twilightUtil.removeTwilightEffects(caster);
+    }
+};
+
+
+
+
