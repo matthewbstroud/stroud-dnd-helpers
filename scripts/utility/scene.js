9e7a15b16577277704da4cb27f424e6b25390f01
Matthew Stroud <mbstroud@gmail.com>
2024-01-05 09:02:14 -0500

    # v1.11.4 # New Features - utility.scene.packAdventureThumbnails - utility.dumpDependencies

--- scripts/utility/scene.js
+++ scripts/utility/scene.js
@@ -1,6 +1,7 @@
 
 export let scene = {
-    "rewireMonksActiveTiles": _rewireMonksActiveTiles
+    "rewireMonksActiveTiles": _rewireMonksActiveTiles,
+    "packAdventureThumbnails": _packAdventureThumbnails
 };
 
 async function _rewireMonksActiveTiles() {
@@ -18,4 +19,32 @@ async function _rewireMonksActiveTiles() {
         });
     });
     ui.notifications.notify(`Processed tile count was ${tileCount}.`);
+}
+
+async function _packAdventureThumbnails(moduleId){
+    const thumbs_folder = 'images/thumbs';
+    if (! await FilePicker.browse('data', `modules/${moduleId}/`).then(r => !r.dirs.includes(`modules/${moduleId}/${thumbs_folder}`))) { 
+        await FilePicker.createDirectory('data', `modules/${moduleId}/${thumbs_folder}/`) 
+    }
+    for (const pack of game.packs.filter(p => p.metadata.packageName === moduleId & p.metadata.type === 'Adventure')) {
+        for (const adventureIdx of pack.index.values()){
+            let adventure = await pack.getDocument(adventureIdx._id);
+            let scenes = Array.from(adventure.scenes);
+            for (var scene of scenes) {
+                let fileName = scene.thumb.split('/').pop();
+                let moduleThumbPath = `modules/${moduleId}/${thumbs_folder}/${fileName}`;
+                if (await srcExists(moduleThumbPath)){
+                    continue;
+                }
+                let blob = await fetch(scene.thumb).then(r => r.blob())
+                let {path} = await FilePicker.upload(
+                    "data", 
+                    `modules/${moduleId}/${thumbs_folder}/`, 
+                    new File([blob], fileName, {type: blob.type}), 
+                    {}, 
+                    {notify:false}
+                );
+            }
+        }
+    }
 }
--- scripts/utility/utility.js
+++ scripts/utility/utility.js
@@ -4,5 +4,10 @@ export let utility = {
     "scene": scene,
     "getControlledActor": function _getControlledActor() {
         return canvas?.tokens?.controlled[0]?.actor
+    },
+    "dumpDependencies": async function _dumpDependencies() {
+        var activemodules = await game.modules.map(m => ({ "id": m.id, "type": "module", "manifest": m.manifest, "comaptibility": { "mininum": m.version, "verified": m.version}}));
+        console.log(JSON.stringify(activemodules, null, 2));
+        return true;
     }
 };
